e06dea35f6d5b80c0c9ddf7365625af9105b2c9d
georgGrankvist <62744506+georgGrankvist@users.noreply.github.com>
2022-02-28 15:37:33 +0100

    node class

---
+++ Ms_Pacman/src/pacman/entries/pacman/Node.java
@@ -0,0 +1,30 @@
+package pacman.entries.pacman;
+
+import java.io.BufferedWriter;
+import java.util.HashMap;
+
+public class Node {
+
+    private String nodeLabel;
+    public HashMap <String, Node> nodeChildren = new HashMap<String,Node>();
+
+    public Node () {
+    }
+
+    public Node (String nodeLabel) {
+        this.nodeLabel = nodeLabel;
+    }
+
+    public void setLabel (String label) {
+        this.nodeLabel = label;
+    }
+
+    public void addChild(String edgeLabel,Node destinationVertex) {
+        nodeChildren.put(edgeLabel,destinationVertex);
+    }
+
+    public Node getChild(String key) {
+        return nodeChildren.get(key);
+    }
+
+}
